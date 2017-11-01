b29cb8cac76c9da55c9e6517335d8b80ad143d9f
howiep <hawkarp@gmail.com>
2017-11-01 15:33:26 +0100

    test

--- JWTtest/Startup.cs
+++ JWTtest/Startup.cs
@@ -11,7 +11,8 @@ using Microsoft.Extensions.Options;
 using JWTtest.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
-
+using Microsoft.AspNetCore.Identity;
+using System.Net;
 
 namespace JWTtest
 {
@@ -27,36 +28,42 @@ namespace JWTtest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie();
+            var hostname = Environment.GetEnvironmentVariable("SQLSERVER_HOST") ?? "localhost";
+            var password = Environment.GetEnvironmentVariable("SQLSERVER_SA_PASSWORD") ?? "kode123@";
+            var connString = $"Data Source={hostname};Initial Catalog=JWTtest;User ID=sa;Password={password};";
 
-            services.AddDbContext<IdentityContext>(opt => opt.UseInMemoryDatabase("IdentityContext"));
-            services.AddIdentity<User, Role>(cfg =>
-            {
-                cfg.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents
-                {
-                    OnRedirectToLogin = ctx =>
-                    {
-                        if (ctx.Request.Path.StartsWithSegments("/api"))
-                        {
-                            ctx.Response.StatusCode = (int)System.Net.HttpStatusCode.Unauthorized;
-                        }
-                        return Task.FromResult(0);
-                    }
-                };
-            }).AddEntityFrameworkStores<IdentityContext>()
+            services.AddDbContext<IdentityContext>(options => options.UseSqlServer(Configuration.GetConnectionString(connString), sqlOptions => sqlOptions.MigrationsAssembly("TokenAuthWebApiCore.server")));
+
+            services.AddIdentity<User, Role>()
+            .AddEntityFrameworkStores<IdentityContext>()
             .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie( options => {
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    if (context.Request.Path.StartsWithSegments("/api"))
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    }
+                    return Task.FromResult(0);
+                };
+            });
+            
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
